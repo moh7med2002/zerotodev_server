58d31ac62dba794e77245141d11ad9da2dc69618
moh7amed2002 <moha7medheshamabohani@gmail.com>
2025-05-07 11:00:08 +0300

    user for admin

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,17 @@
-import { Body, Controller, Get, Patch, Post, Put, UploadedFile, UseFilters, UseGuards, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+  Query,
+  UploadedFile,
+  UseFilters,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { createUserDto } from './dto/create-user.dto';
 import { Serilaize } from 'src/common/interceptors/serialize.interceptor';
@@ -12,6 +25,7 @@ import { UserGuard } from 'src/guards/user.guard';
 import { createImageInterceptor } from 'src/common/interceptors/createImage.interceptor';
 import { MulterExceptionFilter } from 'src/common/filters/multerException.filter';
 import { UserProfileDto } from './dto/user-profile.dto';
+import { AdminGuard } from 'src/guards/admin.guard';
 
 @Controller('user')
 export class UserController {
@@ -20,33 +34,32 @@ export class UserController {
   @Post('signup')
   @UseInterceptors(createImageInterceptor('image', 'images'))
   @UseFilters(MulterExceptionFilter)
-  async signUpUser(@Body() body:createUserDto,@UploadedFile() file?: Express.Multer.File)
-  {
+  async signUpUser(
+    @Body() body: createUserDto,
+    @UploadedFile() file?: Express.Multer.File,
+  ) {
     const imageUrl = file ? `/images/${file.filename}` : undefined;
-    return this.userService.signUp(body,imageUrl)
+    return this.userService.signUp(body, imageUrl);
   }
 
   @Post('login')
-  async loginUser(@Body() body:loginUserDto)
-  {
-    return this.userService.login(body)
+  async loginUser(@Body() body: loginUserDto) {
+    return this.userService.login(body);
   }
 
   @UseGuards(UserGuard)
   @Serilaize(UserDto)
   @Patch('email')
   @UseGuards(UserGuard)
-  changeUserEmail(@Body() body:UserEmailDto,@CurrentUser() user:User)
-  {
-    return this.userService.changeEmail(body.newEmail,+user.id)
+  changeUserEmail(@Body() body: UserEmailDto, @CurrentUser() user: User) {
+    return this.userService.changeEmail(body.newEmail, +user.id);
   }
-  
+
   @Serilaize(UserDto)
   @Patch('password')
   @UseGuards(UserGuard)
-  changeUserPassword(@Body() body:UserPasswordDto,@CurrentUser() user:User)
-  {
-      return this.userService.changePassword(body,+user.id)
+  changeUserPassword(@Body() body: UserPasswordDto, @CurrentUser() user: User) {
+    return this.userService.changePassword(body, +user.id);
   }
 
   @Serilaize(UserDto)
@@ -56,18 +69,31 @@ export class UserController {
   @UseFilters(MulterExceptionFilter)
   updateProfile(
     @Body() body: Partial<User>,
-    @CurrentUser() user:User,
+    @CurrentUser() user: User,
     @UploadedFile() file?: Express.Multer.File,
   ) {
     const imageUrl = file ? `/images/${file.filename}` : undefined;
-    return this.userService.update(body,user.id, imageUrl);
+    return this.userService.update(body, user.id, imageUrl);
   }
 
   @Serilaize(UserProfileDto)
   @UseGuards(UserGuard)
   @Get('current')
-  getCurrentUser(@CurrentUser() user:User)
-  {
-    return user ;
+  getCurrentUser(@CurrentUser() user: User) {
+    return user;
+  }
+
+  @UseGuards(AdminGuard)
+  @Get('/all')
+  getAll(
+    @Query('page') page: string = '1',
+    @Query('limit') limit: string = '5',
+  ) {
+    return this.userService.getAllUsers(+page, +limit);
+  }
+
+  @Get('admin/:userId')
+  getAdminProfile(@Param('id') id: string) {
+    return this.userService.getUserProfileForAdmin(+id);
   }
 }
