1d085afaab2d003a9f214ad27df7e18a9b7f06fb
moh7amed2002 <moha7medheshamabohani@gmail.com>
2025-05-14 11:49:12 +0300

    dto

--- src/modules/skill/dto/create-skill.dto.ts
+++ src/modules/skill/dto/create-skill.dto.ts
@@ -1,7 +1,8 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
 
 export class CreateSkillDto {
   @IsNotEmpty()
   @IsString()
+  @MaxLength(15, { message: 'يجب ان لا تزيد اسم المهارة عن 15 حرفا' })
   title: string;
 }
