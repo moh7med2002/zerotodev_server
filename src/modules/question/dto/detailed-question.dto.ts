c284331bc719669e80649c0f60a1726ca21bf859
moh7amed2002 <moha7medheshamabohani@gmail.com>
2025-05-22 11:22:58 +0300

    get article and question for admin

--- src/modules/question/dto/detailed-question.dto.ts
+++ src/modules/question/dto/detailed-question.dto.ts
@@ -1,27 +1,32 @@
-import { Expose } from 'class-transformer';
+import { Expose, Type } from 'class-transformer';
+import { CommentDto } from 'src/modules/comment/dto/comment.dto';
 
 export class DetailedQuestionDto {
-    @Expose()
-    id: number;
+  @Expose()
+  id: number;
 
-    @Expose()
-    title: string;
+  @Expose()
+  title: string;
 
-    @Expose()
-    headline: string;
+  @Expose()
+  headline: string;
 
-    @Expose()
-    content: string;
+  @Expose()
+  content: string;
 
-    @Expose()
-    status: string;
+  @Expose()
+  status: string;
 
-    @Expose()
-    publish_date: Date;
+  @Expose()
+  publish_date: Date;
 
-    @Expose()
-    views: number;
+  @Expose()
+  views: number;
 
-    @Expose()
-    commentCount: number;
+  @Expose()
+  commentCount: number;
+
+  @Expose()
+  @Type(() => CommentDto)
+  comments: CommentDto[];
 }
